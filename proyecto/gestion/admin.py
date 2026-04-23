97d714d6200e7d94a33c340271afe28290ffe8a8
Andresittow <edison.chacuaq@campusucc.edu.co>
2026-04-23 09:18:54 -0500

    Subiendo urls de app y de proyecto

--- proyecto/gestion/admin.py
+++ proyecto/gestion/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from .models import Autor, Libro
 
-# Register your models here.
 @admin.register(Autor)
 class AutorAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'correo', 'nacionalidad', 'fecha_nacimiento')
+
 @admin.register(Libro)
 class LibroAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'genero', 'fecha_publicacion', 'autor', 'isbn')
